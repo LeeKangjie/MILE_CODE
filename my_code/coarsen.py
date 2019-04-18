e8a19db0a7822132c9d9ec01282d02b1c850f5f8
xidianzyf <henuzyf@qq.com>
2019-04-18 22:02:33 +0800

    update

--- my_code/coarsen.py
+++ my_code/coarsen.py
@@ -1,5 +1,5 @@
-from Entity.graph import Graph
 import numpy as np
+
 
 def coarsen(origin_graph, times=3):
 	list = []
@@ -13,9 +13,21 @@ def coarsen(origin_graph, times=3):
 def get_coarsen_graph(graph):
 	new_graph = None
 	adj = graph.adj_mat
+	dgeree = graph.degree_mat
 	mapper = {}
-	for i in graph.node_num:
+	for i in range(graph.node_num):
 		mapper[i] = i
+	flag = np.zeros(graph.nodenum, dtype=np.int)
+	# mile 中第一种粗化方法
+	for i in range(graph.node_num):
+		for j in range(graph.node_num):
+			if dgeree[i][i] == dgeree[j][j] and dgeree[i][i] == 1 and np.where(adj[i] == 1) == np.where(adj[j] == 1) \
+					and flag[i] == 0 and flag[j] == 0:
+				flag[i] = 1
+				flag[j] = 1
+				dgeree[np.where(adj[i] == 1)[0]] -= 1
+			# TODO ij 改为映射的方法
 
+	# mile 中第二种粗化方法
 
 	return new_graph
